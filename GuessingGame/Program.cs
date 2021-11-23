998d51f7fdbba0c67c63961019d7f80e19c16020
Rysiek <rysiexo2@gmail.com>
2021-11-23 22:37:48 +0100

    Splitted two games in seperate functions.

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -6,25 +6,75 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            int number;
+
+
+            Console.WriteLine("Hi let's play a game.\nType 1 to go solo, against pseudo-number genernator");
+            Console.WriteLine("Type 2 to play with you friend.");
+
+            int chose = Convert.ToInt32(Console.ReadLine());
+
+            switch(chose)
+            {
+                case 1:
+                    SoloGame();
+                    break;
+                case 2:
+                    TwoplayerGame();
+                    break;
+            }
+
+            
+        }
+
+        static void SoloGame()
+        {
+            Console.WriteLine("Guess a number");
+            Random r = new Random();
             int guesses = 1;
-            Random random = new Random();
-
-            Console.Write("Let's play a game.\nIt's number guessing game.\nGive us a number - the higher the better.\nFor optimal performance, suggested number should be higher than 100\n");
-            number = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Let's start the game");
+            int generated = r.Next(0, new Random().Next(100, 1000));
 
             while(true)
             {
-                int n = Convert.ToInt32(Console.ReadLine());
-                if(n == number)
+                int number = Convert.ToInt32(Console.ReadLine());
+                if(number == generated)
                 {
                     Console.WriteLine("Congratulations, you win");
-                    Console.WriteLine("Number of tries you took: {0}", guesses);
+                    Console.WriteLine("Number of guesses: {0}", guesses);
+                }
+                Console.WriteLine("Oops, you missed. Try again");
+                ++guesses;
+            }
+        }
+
+        static void TwoplayerGame()
+        {
+            Console.WriteLine("2 player game, best way played with a number higher than 100\n");
+            int number;
+            int guesses = 1;
+            string value = null;
+
+            while (true)
+            {
+                var key = Console.ReadKey(true).KeyChar;
+                if (key == 13)
+                    break;
+                value += key;
+            }
+            number = Convert.ToInt32(value);
+
+            Console.WriteLine("Let's start the game");
+
+            while (true)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                if (n == number)
+                {
+                    Console.WriteLine("Congratulations, you win");
+                    Console.WriteLine("Number of guesses: {0}", guesses);
                     break;
                 }
-                Console.WriteLine("We're sorry, you missed. Try as much as you want. There is no counter");
+                Console.WriteLine("We're sorry, you missed. Try again");
                 ++guesses;
             }
         }
